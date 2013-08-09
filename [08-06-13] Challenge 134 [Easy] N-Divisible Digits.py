be17701d68389fd35fe27fb647142ed2db004ce9
GregHamel <greghamel@gmail.com>
2013-08-08 20:59:54 -0500

    2 new challenges completed, 1 supporting txt file

---
+++ [08-06-13] Challenge 134 [Easy] N-Divisible Digits.py
@@ -0,0 +1,22 @@
+
+#[08/06/13] Challenge #134 [Easy] N-Divisible Digits
+
+import time
+
+def n_digits(N,M):
+    for x in range(int("9"*N),0,-1):
+        if x%M == 0 and len(str(x)) == N:
+            return x
+    return None
+
+start = time.clock()
+
+def n_digits(N,M):
+    return int("9"*N) - (int("9"*N) %M)
+
+print (n_digits(9,641351511))
+
+
+stop = time.clock()
+print(stop-start)
+
